bbedac3f7db35be822f95869b9e679d1e62faa70
Nguyen Minh Quang <minhquang10998@gmail.com>
2024-11-02 22:09:23 +0700

    Done memoized

--- Function_Transformations/Memoize.js
+++ Function_Transformations/Memoize.js
@@ -3,44 +3,32 @@
  * @return {Function}
  */
 function memoize(fn) {
-  handleSum = (a, b) => {
-      return a ;
-  }
-
-  handleFib = (n) => {
-      if (n > 1) {
-          return (n - 1) + (n - 2);
-      } else {
-          return 1;
-      }
-  }
-
-  handleFactorial = (n) => {
-      if (n > 1) {
-          return (n - 1) * n;
-      } else {
-          return 1;
-      }
-  }
-
-  return function(...args) {
-      switch (fn) {
-          case "sum": return handleSum(...args);
-          case "fib": return handleFib(...args);
-          case "factorial": return hanldeFactorial(...args);
-          default: return;
-      }
+    const cache = {};
+  
+   return function(...args) {
+    const key = JSON.stringify(args);
+    
+    // check xem biến đã được truyền bao giờ chưa
+    // nếu có thì return kết quả đã lưu
+    if (key in cache) {
+        return cache[key];
+    }
+      
+    const result = fn(...args);
+    cache[key] = result;
+      
+    return result;
   }
 }
 
 
 /** 
-* let callCount = 0;
-* const memoizedFn = memoize(function (a, b) {
-*	 callCount += 1;
-*   return a + b;
-* })
-* memoizedFn(2, 3) // 5
-* memoizedFn(2, 3) // 5
-* console.log(callCount) // 1 
-*/
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
