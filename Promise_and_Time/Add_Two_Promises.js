edd52997cde3ff5712785760a873e27bb995d441
Nguyen Minh Quang <minhquang10998@gmail.com>
2024-11-19 22:44:39 +0700

    done add two promise

--- Promise_and_Time/Add_Two_Promises.js
+++ Promise_and_Time/Add_Two_Promises.js
@@ -3,18 +3,16 @@
  * @param {Promise} promise2
  * @return {Promise}
  */
-
-// Undone
 var addTwoPromises = async function(promise1, promise2) {
-  let result1 = 0;
-  let result2 = 0;
-  promise1.then((value) => {
-      result1 = value
+  let sum = 0;
+  return new Promise(resolve => {
+      Promise.all([promise1, promise2]).then((values) => {
+          values.forEach( num => {
+              sum += num;
+          })
+          resolve(sum)
+      });
   })
-  promise2.then((value) => {
-      result2 = value
-  })
-  return new Promise(resolve => setTimeout(() => resolve(result1 + result2), 80))
 };
 
 /**
