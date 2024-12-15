1e845994ba2c0783d462bf91bbe8d1b427ad6501
Nguyen Minh Quang <minhquang10998@gmail.com>
2024-12-15 21:10:02 +0700

    done

---
+++ Promise_and_Time/Cache_With_Time_Limit.js
@@ -0,0 +1,47 @@
+var TimeLimitedCache = function() {
+  this.listItem = new Map();
+};
+
+/** 
+* @param {number} key
+* @param {number} value
+* @param {number} duration time until expiration in ms
+* @return {boolean} if un-expired key already existed
+*/
+TimeLimitedCache.prototype.set = function(key, value, duration) {
+  const found = this.listItem.has(key);
+  if (found) {
+     clearTimeout(this.listItem.get(key).time);
+  }
+  this.listItem.set(key, {
+      value: value,
+      time: setTimeout(() => this.listItem.delete(key), duration)
+  });
+  return found;
+};
+
+/** 
+* @param {number} key
+* @return {number} value associated with key
+*/
+TimeLimitedCache.prototype.get = function(key) {
+  if (this.listItem.has(key)) {
+      return this.listItem.get(key).value;
+  } else {
+      return -1;
+  }
+};
+
+/** 
+* @return {number} count of non-expired keys
+*/
+TimeLimitedCache.prototype.count = function() {
+  return this.listItem.size;
+};
+
+/**
+* const timeLimitedCache = new TimeLimitedCache()
+* timeLimitedCache.set(1, 42, 1000); // false
+* timeLimitedCache.get(1) // 42
+* timeLimitedCache.count() // 1
+*/
